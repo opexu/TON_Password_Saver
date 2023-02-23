d139e9ba2c9a66e3a9fe54ecf475d37c0b4be187
opexu <opexu1986@gmail.com>
2023-02-23 22:29:15 +0300

    +++

--- webapp/src/scripts/WalletConnection.ts
+++ webapp/src/scripts/WalletConnection.ts
@@ -27,6 +27,8 @@ export class WalletConnection implements IConnection {
         this._status = ConnectionStatus.DISABLE;
         this._connector = new TonConnect({ manifestUrl: 'https://raw.githubusercontent.com/opexu/TON_Password_Saver/main/webapp/src/tonconnect-manifest.json'});    
         this._connector.onStatusChange( this._onStatusChange.bind( this ) );
+
+        this._connector.restoreConnection();
     }
     get debug(){ return this._debug; }
     get deepLink(){ return this._deepLink; }
