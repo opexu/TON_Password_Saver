7693b62820892c7373c00f00f2a3f33b7b7161c5
opexu <opexu1986@gmail.com>
2023-03-12 00:14:48 +0300

    '+++'

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="w-full h-full flex flex-col items-center content-between">
+<div class="w-full h-full flex flex-col items-center justify-between">
     
     <TonConnect
     :Locale="Locale"
